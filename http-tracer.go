minio/sidekick

fix: healthcheck should be skipped for 'minio' trace

http-tracer.go
@@ -315,7 +315,7 @@ func doTrace(trace TraceInfo, backend *Backend) {
 		return
 	}
 
-	if globalTrace == "minio" && strings.Contains(backend.healthCheckURL, st.Path) {
+	if globalTrace == "minio" && !strings.Contains(backend.healthCheckURL, st.Path) {
 		return
 	}
 

targetaidev/sideweed

http-tracer.go
@@ -315,7 +315,7 @@ func doTrace(trace TraceInfo, backend *Backend) {
 		return
 	}
 
-	if globalTrace == "cluster" && strings.Contains(backend.healthCheckURL, st.Path) {
+	if globalTrace == "cluster" && !strings.Contains(backend.healthCheckURL, st.Path) {
 		return
 	}
 